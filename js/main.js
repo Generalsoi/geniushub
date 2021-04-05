d3eb624ff099d0e0b00ac6bb8459ced54dac8bbc
Generalsoi <ikhinobelesuccess@gmail.com>
2021-04-05 07:07:25 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ const navToggle = document.getElementById("navToggle")
 const nav = document.querySelector("nav")
 const navIcon = document.querySelectorAll('.navIcon')
 const hamburger = document.querySelector("#hamburger")
+const submenu = document.querySelector("nav ul li")
+const submenus = document.querySelector("nav ul li ul")
 
 navToggle.addEventListener('click', function () {
     nav.classList.toggle('open')
     navIcon.forEach(icon => {
         icon.classList.toggle('hidden')
     })
+})
+
+submenu.addEventListener('click', function () {
+    /*this.siblings.classList.remove('active')
+    this.classList.toggle('active')*/
+    submenus.style.display = 'block'
 })
 
 window.addEventListener("resize", () => {
